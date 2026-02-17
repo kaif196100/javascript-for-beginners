67f39e1ffdd4f6ee811c060d0539cf19d389c03c
kaif196100 <mohammadkaifk110@gmail.com>
2026-02-17 22:14:22 +0530

    Add files via upload

---
+++ 02_Array.js
@@ -0,0 +1,10 @@
+let heroes = ["ironman", "thor", "hulk", "antman"];
+// for(i=0; i < heroes; i++) {  // for loop
+//     console.log(heroes[i]);
+// }
+
+// for of
+
+for(let hero of heroes) {
+    console.log(hero);
+}
