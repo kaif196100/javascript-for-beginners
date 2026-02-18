ea4c8adfc184e83c731118f974e82ac5e69c8cd0
kaif196100 <mohammadkaifk110@gmail.com>
2026-02-18 11:39:13 +0530

    Add files via upload

---
+++ 01_ifelse.js
@@ -0,0 +1,21 @@
+let marks = 96;
+
+if (marks >= 91 /*|| marks >= 100*/) {
+    console.log("garde A+");
+}
+else if (marks >= 80) {
+    console.log("garde A");
+}
+else if (marks >= 70) {
+    console.log("gards B")
+}
+else if (marks >= 60) {
+    console.log("gards C");
+}
+else if (marks >= 50) {
+    console.log("gards D");
+}
+else  {
+    console.log("fail");
+}
+
