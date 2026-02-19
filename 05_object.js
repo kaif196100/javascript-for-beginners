2042f6203c9d4f66622b516b14af489369306fc3
Mohammed kaif <mohammadkaifk110@gmail.com>
2026-02-19 22:03:43 +0530

    Add files via upload

---
+++ 05_object.js
@@ -0,0 +1,24 @@
+var x = Math.min(4, 100, 20, -5);
+
+console.log(x);
+
+
+var x = Math.max(4, 100, 20, -5);
+
+console.log(x);
+
+var x = Math.trunc(4.8);
+
+console.log(x);
+
+var x = Math.floor(4.2);
+
+console.log(x);
+
+var x = Math.ceil(4.9);
+
+console.log(x);
+
+var x = Math.round(4.9);
+
+console.log(x);
