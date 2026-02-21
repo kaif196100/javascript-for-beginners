5d13b3005c686cebb124910369b74873a940674e
Mohammed kaif <mohammadkaifk110@gmail.com>
2026-02-21 13:18:33 +0530

    Add files via upload

---
+++ 09_objectNested.js
@@ -0,0 +1,32 @@
+var user = {
+    id: 124,
+    email: "abc@gmail.com",
+    personalInfo: {
+        name: "abc",
+        address: {
+            streey: "djdjdj",
+            city: "jodhpur",
+            country: "India"
+        }
+    }
+};
+
+console.log(user);
+
+// personalInfo ke liya 
+
+var user = {
+    id: 124,
+    email: "abc@gmail.com",
+    personalInfo: {
+        name: "abc",
+        address: {
+            streey: "djdjdj",
+            city: "jodhpur",
+            country: "India"
+        }
+    }
+};
+
+console.log(user.personalInfo);
+
