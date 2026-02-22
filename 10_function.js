a1c2f35920fb3e402b4ae00a2f1b267daab9bab7
Mohammed Kaif <mohammadkaifk110@gmail.com>
2026-02-22 13:19:10 +0530

    Add files via upload

---
+++ 10_function.js
@@ -0,0 +1,17 @@
+
+// compare karna 
+
+function compare(x,y){
+  if ( x > y){
+   return 1;
+  }
+  else if (y > x){
+   return -1;
+  }
+  else{
+   return 0;
+  }
+}
+let c = compare (8, 6);
+
+console.log(c)
